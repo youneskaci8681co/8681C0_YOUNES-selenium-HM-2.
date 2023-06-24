235a5d8eb60ce50f184b8330ff054e06b90009a2
youneskaci8681co <youneskaci388@yahoo.com>
2023-06-23 20:06:12 -0700

    user registration test

--- src/test/java/test_app/TestApp.java
+++ src/test/java/test_app/TestApp.java
@@ -10,17 +10,54 @@ import static org.openqa.selenium.By.xpath;
 public class TestApp extends BasePage {
 
 
-    @Test (priority = 0, groups = {"BAT"})
-    public void testNavigationToApplication() {
-        Homepage homepage = new Homepage();
+    import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-        public void testSuccessfulRegistration () {
-            // Test Data
-            String name = "YOUNES";
-            String email = "roni_cost@example";
-            String password = "roni_cost3@example.com";
+import java.util.concurrent.TimeUnit;
 
+    public class UserRegistrationTest {
+        private WebDriver driver;
 
+        @BeforeMethod
+        public void setUp() {
+            // Set up the WebDriver instance (e.g., ChromeDriver)
+            System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+            driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        }
+
+        @AfterMethod
+        public void tearDown() {
+            // Quit the WebDriver instance
+            driver.quit();
+        }
+
+        @Test
+        public void userRegistrationTest() {
+            // Test Steps
+            driver.get("https://magento.softwaretestingboard.com/");
+            driver.findElement(By.linkText("Sign In")).click();
+            driver.findElement(By.linkText("Create an Account")).click();
+            driver.findElement(By.id("firstname")).sendKeys("younes");
+            driver.findElement(By.id("lastname")).sendKeys("kaci");
+            driver.findElement(By.id("email_address")).sendKeys("roni_cost@example.com");
+            driver.findElement(By.id("password")).sendKeys("roni_cost3@exemple.com");
+            driver.findElement(By.id("confirmation")).sendKeys("roni_cost3@exemple.com");
+            driver.findElement(By.cssSelector("button[title='Register']")).click();
+
+            // Expected Results
+            WebElement dashboardHeading = driver.findElement(By.cssSelector("h1.dashboard"));
+            Assert.assertTrue(dashboardHeading.isDisplayed());
+            Assert.assertEquals(dashboardHeading.getText(), "My Dashboard");
+            WebElement welcomeMessage = driver.findElement(By.cssSelector("p.hello"));
+            Assert.assertTrue(welcomeMessage.isDisplayed());
+            Assert.assertEquals(welcomeMessage.getText(), "Hello, younes kaci!");
         }
     }
 
